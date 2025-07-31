decd7586ffe5dd47724a2a7e373591e9e95434c7
Hamza3270308 <hami5511@gmail.com>
2025-07-31 16:31:13 +0500

    Infinite.lua

--- Infinite.lua
+++ Infinite.lua
@@ -119,7 +119,8 @@ end
 -- Run the script after key verification
 local function runMainScript()
     -- Your original script logic here
-    local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+    local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohello687/SoleHubBronx3/main/thebronx3.txt"))()
+
 
     for PlaceID, Execute in pairs(Games) do
         if PlaceID == game.PlaceId then
